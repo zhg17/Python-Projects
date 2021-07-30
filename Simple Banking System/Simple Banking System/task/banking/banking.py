dfd7461032dbff4e1dbe15095484c72644b01cf9
zhg17 <ianguo25@gmail.com>
2021-07-29 20:04:35 -0400

    card anatomy finished

--- Simple Banking System/Simple Banking System/task/banking/banking.py
+++ Simple Banking System/Simple Banking System/task/banking/banking.py
@@ -24,7 +24,67 @@ class Account:
         return self.pin
 
 
-account = Account()
-print(account.get_balance())
-print(account.get_card_number())
-print(account.get_pin())
+class System:
+    def __init__(self):
+        self.all_accounts = []
+        self.accounts_info = {}
+
+    def start_system(self):
+        while True:
+            print("1. Create an account")
+            print("2. Log into account")
+            print("0. Exit")
+            choice = int(input())
+            if choice == 1:
+                System.create_account(self)
+            elif choice == 2:
+                print("Enter your card number:")
+                card_num = input()
+                print("Enter your PIN:")
+                pin = input()
+                System.login_account(self, card_num, pin)
+            else:
+                print('Bye!')
+                exit()
+
+    def create_account(self):
+        new_account = Account()
+        self.all_accounts.append(new_account)
+        self.accounts_info[new_account.get_card_number()] = new_account
+        print('Your card number:')
+        print(new_account.get_card_number())
+        print('Your card PIN:')
+        print(new_account.get_pin())
+
+    def check_account(self, card_number, pin):
+        for ele in self.all_accounts:
+            if ele.get_card_number() == card_number:
+                if ele.get_pin() == pin:
+                    return True
+        return False
+
+    def login_account(self, card_number, pin):
+        successful = System.check_account(self, card_number, pin)
+        if not successful:
+            print("Wrong card number or PIN!")
+        else:
+            print("You have successfully logged in!")
+            System.logged_in(self, card_number)
+
+    def logged_in(self, card_number):
+        print("1. Balance")
+        print("2. Log out")
+        print("0. Exit")
+        choice = input()
+        if choice == 1:
+            print("Balance:", self.accounts_info[card_number].get_balance())
+        elif choice == 2:
+            print('You have successfully logged out!')
+            System.start_system(self)
+        else:
+            print('Bye!')
+            exit()
+
+
+system = System()
+system.start_system()
