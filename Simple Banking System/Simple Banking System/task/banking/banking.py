0ea964d424c5ec088d0f8c8c13193561b9690373
zhg17 <ianguo25@gmail.com>
2021-08-02 14:54:51 -0400

    Sqlite3 Added to implementation

--- Simple Banking System/Simple Banking System/task/banking/banking.py
+++ Simple Banking System/Simple Banking System/task/banking/banking.py
@@ -1,4 +1,23 @@
 import random
+import sqlite3
+
+
+def generate_checksum(identifier):
+    # multiply the odd digits by 2
+    identifier_lst = list(map(int, str(identifier)))
+    for i, val in enumerate(identifier_lst):
+        if i % 2 == 0:
+            identifier_lst[i] = identifier_lst[i] * 2
+    # subtract 9 from numbers over 9
+    for i, val in enumerate(identifier_lst):
+        if val > 9:
+            identifier_lst[i] = val - 9
+    # add all numbers
+    sum_of_digits = 0
+    for i, val in enumerate(identifier_lst):
+        sum_of_digits += val
+    checksum = 10 - (sum_of_digits % 10)
+    return checksum
 
 
 class Account:
@@ -8,34 +27,16 @@ class Account:
         sample_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         account_number = str(random.sample(sample_list, 9))
         card_number = '400000' + str(account_number.strip("[]")).replace(',', '').replace(' ', '')
-        checksum = Account.generate_checksum(self, int(card_number))
+        checksum = generate_checksum(int(card_number))
         # checking if the checksum is only 1 digit, if not, randomly generate the identifier again
         while checksum > 9:
             account_number = str(random.sample(sample_list, 9))
             card_number = '400000' + str(account_number.strip("[]")).replace(',', '').replace(' ', '')
-            checksum = Account.generate_checksum(self, int(card_number))
+            checksum = generate_checksum(int(card_number))
         self.card_number = card_number + str(checksum)
         self.pin = str(random.sample(sample_list, 4)) \
             .strip("[]").replace(',', '').replace(' ', '')
         self.balance = 0
-
-
-    def generate_checksum(self, identifier):
-        # multiply the odd digits by 2
-        identifier_lst = list(map(int, str(identifier)))
-        for i, val in enumerate(identifier_lst):
-            if i % 2 == 0:
-                identifier_lst[i] = identifier_lst[i] * 2
-        # subtract 9 from numbers over 9
-        for i, val in enumerate(identifier_lst):
-            if val > 9:
-                identifier_lst[i] = val - 9
-        # add all numbers
-        sum_of_digits = 0
-        for i, val in enumerate(identifier_lst):
-            sum_of_digits += val
-        checksum = 10 - (sum_of_digits % 10)
-        return checksum
 
     def get_card_number(self):
         return self.card_number
@@ -51,6 +52,16 @@ class System:
     def __init__(self):
         self.all_accounts = []
         self.accounts_info = {}
+        self.conn = sqlite3.connect('card.s3db')
+        self.curr = self.conn.cursor()
+        self.curr.execute('''CREATE TABLE IF NOT EXISTS card (
+                          id INTEGER,
+                          number TEXT NOT NULL,
+                          pin TEXT NOT NULL,
+                          balance INTEGER DEFAULT 0
+                          );
+                          ''')
+        self.conn.commit()
 
     def start_system(self):
         while True:
@@ -74,15 +85,19 @@ class System:
         new_account = Account()
         self.all_accounts.append(new_account)
         self.accounts_info[new_account.get_card_number()] = new_account
+        self.curr.execute(
+            f"INSERT INTO card(number, pin, balance) VALUES({new_account.get_card_number()}, {new_account.get_pin()}, 0)")
+        self.conn.commit()
         print('Your card number:')
         print(new_account.get_card_number())
         print('Your card PIN:')
         print(new_account.get_pin())
 
     def check_account(self, card_number, pin):
-        for ele in self.all_accounts:
-            if ele.get_card_number() == card_number:
-                if ele.get_pin() == pin:
+        self.curr.execute('SELECT * FROM card')
+        for ele in self.curr.fetchall():
+            if ele[1] == card_number:
+                if ele[2] == pin:
                     return True
         return False
 
@@ -95,12 +110,14 @@ class System:
             System.logged_in(self, card_number)
 
     def logged_in(self, card_number):
+        self.curr.execute(f'SELECT * FROM card WHERE number = {card_number}')
+        card = self.curr.fetchall()
         print("1. Balance")
         print("2. Log out")
         print("0. Exit")
         choice = input()
         if choice == 1:
-            print("Balance:", self.accounts_info[card_number].get_balance())
+            print("Balance:", card[0][3])
         elif choice == 2:
             print('You have successfully logged out!')
             System.start_system(self)
